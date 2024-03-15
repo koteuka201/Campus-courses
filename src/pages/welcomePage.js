463a90231138ed7f5d5c8eeb1f23d60e4a02e89b
Viktor Kuzubov <kuzubovvv@gmail.com>
2024-03-15 20:10:01 +0700

    save

---
+++ src/pages/welcomePage.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { Container, Button, Alert, Card, CardBody, CardTitle,Form, FormGroup, FormControl, FormLabel } from 'react-bootstrap';
+
+
+
+export default function WelcomePage(){
+
+    
+    return (
+        <Container className="mt-5">
+            Добро пожаловать в систему кампусных курсов
+
+ 
+        </Container>
+    );
+}
