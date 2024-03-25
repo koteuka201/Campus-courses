f5396383e19b4a5b9727ac57a6dad1fdd2517e5e
koteuka201 <kuzubovvv@gmail.com>
2024-03-25 18:58:11 +0700

    детали

--- src/components/courseDetails/modals/statusCourseModal.js
+++ src/components/courseDetails/modals/statusCourseModal.js
@@ -1,15 +1,61 @@
 import React, { useState, useEffect } from "react";
-import ReactQuill from 'react-quill';
-import ReactSelect from 'react-select';
-import 'react-quill/dist/quill.snow.css';
 import { Button, Modal, ModalHeader, ModalFooter, ModalBody, ModalTitle, Form, FormGroup, FormLabel, FormControl, FormCheck } from 'react-bootstrap';
 import {  } from "../../services/apiService";
-export default function CreateEditCourseModal ({ show, handleClose, token, id, updateCourses }){
+export default function CreateEditCourseModal ({token, id, status, show, handleClose,  updateCourses }){
     
-    
+    const [selectedStatus, setSelectedStatus]=useState(status)
 
     return (
 
-        <></>
+        <Modal show={show} onHide={handleClose} className="modal-xl">
+            <ModalHeader closeButton>
+                <ModalTitle>
+                    Изменение статуса курса
+                </ModalTitle>
+            </ModalHeader>
+            <ModalBody>
+                <Form>
+                    <FormGroup className="mb-3">
+                        <FormLabel>Семестр</FormLabel>
+                        <div>
+                            <FormCheck
+                                inline
+                                type="radio"
+                                label="Создан"
+                                name="filter"
+                                value="Created"
+                                checked={selectedStatus === "Created"}
+                                onChange={(e)=> 
+                                    setSelectedStatus(e.target.value)
+                                    
+                                }
+                            />
+                            <FormCheck
+                                inline
+                                type="radio"
+                                label="В процессе обучения"
+                                name="filter"
+                                value="Started"
+                                checked={selectedStatus === "Started"}
+                                onChange={(e)=> 
+                                    setSelectedStatus(e.target.value)   
+                                }
+                            />
+                            <FormCheck
+                                inline
+                                type="radio"
+                                label="В процессе обучения"
+                                name="filter"
+                                value="Started"
+                                checked={selectedStatus === "Started"}
+                                onChange={(e)=> 
+                                    setSelectedStatus(e.target.value)   
+                                }
+                            />
+                        </div>
+                    </FormGroup>
+                </Form>
+            </ModalBody>
+        </Modal>
     );
 }
